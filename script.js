c6721d4c4ec41f8f412875dfd036639347f760a8
majemi97 <167489556+majemi97@users.noreply.github.com>
2024-05-17 11:22:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -187,7 +187,8 @@ const setPlayerCards = (arr = players) => {
     ({ name, position, number, isCaptain, nickname }) => {
       `
         <div class="player-card">
-      <h2>${name}</h2>
+          <h2>${isCaptain ? "(Captain)" : ""} ${name}</h2>
+          
         </div>
       `;
     }
