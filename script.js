b10bfb4224358161e85e2129c19a9370d7d25973
majemi97 <167489556+majemi97@users.noreply.github.com>
2024-05-17 10:00:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -183,10 +183,11 @@ headCoach.textContent = coachName;
 const setPlayerCards = (arr = players) => {
 
 
-  playerCards.innerHTML += arr.map(({name, position, number, isCaptain, nickname}) => {
-
-  })
+  playerCards.innerHTML += arr.map(
+    ({ name, position, number, isCaptain, nickname }) => {
+`<div class="player-card"></div>`
+    }
+  );
 
 
 };
-
