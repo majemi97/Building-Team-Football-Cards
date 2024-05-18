88e09a46ad87bc41600f9c7e1d77b5a7531a92ca
majemi97 <167489556+majemi97@users.noreply.github.com>
2024-05-17 19:10:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -197,9 +197,12 @@ const setPlayerCards = (arr = players) => {
 };
 
 playersDropdownList.addEventListener("change", (e) => {
+  playerCards.innerHTML = "";
 
 
-
+  switch (e.target.value) {
+    
+  }
 
 
 });
