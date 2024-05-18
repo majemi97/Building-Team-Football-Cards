9592287a5f6244e2d38d80d3342c01e8cb4dce94
majemi97 <167489556+majemi97@users.noreply.github.com>
2024-05-17 19:16:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -221,8 +221,8 @@ playersDropdownList.addEventListener("change", (e) => {
         players.filter((player) => player.position === "goalkeeper")
       );
       break;
-
-
-
+    default:
+      setPlayerCards();
+      break;
   }
 });
