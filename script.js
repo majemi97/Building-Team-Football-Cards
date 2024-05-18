be924f6408818f5b0acafa98e7a6a732629349a0
majemi97 <167489556+majemi97@users.noreply.github.com>
2024-05-17 19:11:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -198,12 +198,14 @@ const setPlayerCards = (arr = players) => {
 
 playersDropdownList.addEventListener("change", (e) => {
   playerCards.innerHTML = "";
-  
+
   switch (e.target.value) {
     case "nickname":
       setPlayerCards(players.filter((player) => player.nickname !== null));
       break;
-
+    case "forward":
+      setPlayerCards(players.filter((player) => player.position === "forward"));
+      break;
 
 
 
